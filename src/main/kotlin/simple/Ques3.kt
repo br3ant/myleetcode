6ed643abd3fed73c7c03d0f72f518cc8fb451913
houqiqi <houqiqi@zepp.com>
2022-05-14 22:08:06 +0800

    优化题目3

--- src/main/kotlin/simple/Ques3.kt
+++ src/main/kotlin/simple/Ques3.kt
@@ -6,15 +6,14 @@ fun lengthOfLongestSubstring(s: String): Int {
     var maxLen = 0
     var tabs = mutableListOf<Char>()
 
-
     s.forEach {
         if (tabs.contains(it)) {
             if (tabs.size > maxLen) {
                 maxLen = tabs.size
             }
             val index = tabs.lastIndexOf(it)
-            tabs = tabs.subList(index + 1, tabs.size)
-
+            //窗口管理应该还有更优解
+            tabs = tabs.drop(index + 1).toMutableList()
         }
         tabs.add(it)
     }
